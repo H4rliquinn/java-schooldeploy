2208cbc85091b64c24070a732568b27b64de6454
Jason Sonnichsen <Harliquinn@gmail.com>
2019-10-17 16:40:31 -0400

    Students integrated into User table of Oauth *chairdance*

--- StartHere/src/main/resources/data.sql
+++ StartHere/src/main/resources/data.sql
@@ -4,8 +4,8 @@ FROM studcourses;
 DELETE
 FROM course;
 
-DELETE
-FROM student;
+-- DELETE
+-- FROM student;
 
 DELETE
 FROM instructor;
@@ -23,27 +23,27 @@ INSERT INTO course (courseid, coursename, instructid)
            (5, 'Mobile IOS', 2),
            (6, 'Mobile Android',  3);
 
-INSERT INTO student (studid, studname)
-    VALUES (1, 'John'),
-           (2, 'Julian'),
-           (3, 'Mary'),
-           (4, 'Julian'),
-           (5, 'Tyler'),
-           (6, 'Kim'),
-           (7, 'Juan'),
-           (8, 'Robby'),
-           (9, 'Roberto'),
-           (10, 'Bob'),
-           (11, 'Liz'),
-           (12, 'June'),
-           (13, 'April');
+-- INSERT INTO student (studid, studname)
+--     VALUES (1, 'John'),
+--            (2, 'Julian'),
+--            (3, 'Mary'),
+--            (4, 'Julian'),
+--            (5, 'Tyler'),
+--            (6, 'Kim'),
+--            (7, 'Juan'),
+--            (8, 'Robby'),
+--            (9, 'Roberto'),
+--            (10, 'Bob'),
+--            (11, 'Liz'),
+--            (12, 'June'),
+--            (13, 'April');
 
-INSERT INTO studcourses (studid, courseid)
-    VALUES (1, 1),
-           (1, 4),
-           (2, 2),
-           (3, 3),
-           (3, 1),
-           (3, 6);
+-- INSERT INTO studcourses (studid, courseid)
+--     VALUES (1, 1),
+--            (1, 4),
+--            (2, 2),
+--            (3, 3),
+--            (3, 1),
+--            (3, 6);
 
-alter sequence hibernate_sequence restart with 20;
+alter sequence hibernate_sequence restart with 40;
